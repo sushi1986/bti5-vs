c3ef6e39f7418b2b81254fa5d36fee7372701bb9
sushi_p <p.gesien@gmx.de>
2012-11-19 13:35:29 +0100

    Remotes vollstaendig

--- VSEx03/src/branch_access/ManagerRemote.java
+++ VSEx03/src/branch_access/ManagerRemote.java
@@ -1,27 +1,63 @@
 package branch_access;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import mware_lib.NameServiceImpl;
 import mware_lib.ObjectBroker;
 
 public class ManagerRemote extends Manager {
 
-    NameServiceImpl ns;
-	
-	public ManagerRemote(String serviceHost, int listenPort) {
-    	ObjectBroker ob = ObjectBroker.getBroker(serviceHost, listenPort);
-        this.ns = (NameServiceImpl) ob.getNameService();
-    }
-	
+	NameServiceImpl ns;
+	String name;
+
+	public ManagerRemote(String name, String serviceHost, int listenPort) {
+		this.name = name;
+
+		ObjectBroker ob = ObjectBroker.getBroker(serviceHost, listenPort);
+		this.ns = (NameServiceImpl) ob.getNameService();
+	}
+
 	@Override
 	public String createAccount(String owner) {
 		// TODO Auto-generated method stub
-		return null;
+		String result = null;
+		try {
+			result = ns.callOnResolved(name, "createAccount", owner);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (result != null) {
+			// alles OK
+			return result;
+		} else {
+			// nicht alles OK
+			return null;
+		}
 	}
 
 	@Override
 	public double getBalance(String accountID) {
-		// TODO Auto-generated method stub
-		return 0;
+		String result = null;
+		try {
+			result = ns.callOnResolved(name, "getBalance", accountID);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (result != null) {
+			// alles OK
+			return new Double(result).doubleValue();
+		} else {
+			// nicht alles OK
+			return -1;
+		}
 	}
-
 }
--- VSEx03/src/cash_access/AccountRemote.java
+++ VSEx03/src/cash_access/AccountRemote.java
@@ -1,31 +1,80 @@
 package cash_access;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 import mware_lib.NameServiceImpl;
 import mware_lib.ObjectBroker;
 
 public class AccountRemote extends Account {
-    
-    NameServiceImpl ns;
-    
-    public AccountRemote(String serviceHost, int listenPort) {
-    	ObjectBroker ob = ObjectBroker.getBroker(serviceHost, listenPort);
-        this.ns = (NameServiceImpl) ob.getNameService();
-    }
-    
-    
-    
-    @Override
-    public void deposit(double amount) {
-        
-    }
 
-    @Override
-    public void withdraw(double amount) throws OverdraftException {
-    }
+	NameServiceImpl ns;
+	String name;
 
-    @Override
-    public double getBalance() {
-        return 0;
-    }
+	public AccountRemote(String name, String serviceHost, int listenPort) {
+		this.name = name;
+		ObjectBroker ob = ObjectBroker.getBroker(serviceHost, listenPort);
+		this.ns = (NameServiceImpl) ob.getNameService();
+	}
+
+	@Override
+	public void deposit(double amount) {
+		String result = null;
+		try {
+			result = ns.callOnResolved(name, "deposit", String.valueOf(amount));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (result.equals("void")) {
+			// alles OK
+		} else {
+			// nicht alles OK
+		}
+	}
+
+	@Override
+	public void withdraw(double amount) throws OverdraftException {
+		String result = null;
+		try {
+			result = ns
+					.callOnResolved(name, "withdraw", String.valueOf(amount));
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (result.equals("void")) {
+			// alles OK
+		} else {
+			// nicht alles OK
+		}
+	}
+
+	@Override
+	public double getBalance() {
+		String result = null;
+		try {
+			result = ns.callOnResolved(name, "getBalance");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (result != null) {
+			// alles OK
+			return new Double(result).doubleValue();
+		} else {
+			// nicht alles OK
+			return -1;
+		}
+	}
 
 }
