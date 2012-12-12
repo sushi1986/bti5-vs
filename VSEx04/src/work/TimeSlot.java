e22ef76f9213a2bef75d96a01d09abebb615b8f5
Joseph Noir <joseph.noir@gmail.com>
2012-12-12 14:41:44 +0100

    people sening in their right slot should now be preferred

--- VSEx04/src/work/TimeSlot.java
+++ VSEx04/src/work/TimeSlot.java
@@ -1,44 +1,26 @@
 package work;
 
 public class TimeSlot {
-	private String team;
-	private long eta;
-	private long ata;
-	private int slot;
-	
-	public TimeSlot() {
-		
-	}
-	
-	public int getSlot() {
-		return slot;
-	}
-	
-	public void setSlot(int actualSlot) {
-		this.slot = actualSlot;
-	}
-	
-	public void setEta(long eta) {
-		this.eta = eta;
-	}
+    private String team;
+    private int slot;
 
-	public long getAta() {
-		return ata;
-	}
+    public TimeSlot() {
 
-	public void setAta(long ata) {
-		this.ata = ata;
-	}
+    }
 
-	public String getTeam() {
-		return team;
-	}
+    public int getSlot() {
+        return slot;
+    }
 
-	public long getEta() {
-		return eta;
-	}
+    public void setSlot(int actualSlot) {
+        this.slot = actualSlot;
+    }
 
-	public void setTeam(String string) {
-		this.team = string;
-	}
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String string) {
+        this.team = string;
+    }
 }
